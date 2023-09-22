a31c86d7380e97c0c2f4a938a1ea99b9e0f3b196
PARZIVAL0000 <dennis-ponce07@outlook.com>
2023-09-22 00:30:48 -0500

    [-] dennis cambio

--- router.py
+++ router.py
@@ -17,55 +17,52 @@ def buscar():
     datos = dict()
 
     if(request.method == 'POST'):
-        buscarPor = request.form['BuscarPor']
         habitaciones = request.form['Habitaciones']
         parqueadero = request.form['Parqueadero']
         tipoAcabados = request.form['TipoAcabados']
         sector = request.form['Sector']
+        barrio = request.form['barrio']
 
-        if(buscarPor != "" and habitaciones != "" and parqueadero != "" and tipoAcabados != "" and sector != ""):
-            
-            #Acomodamos nuestras variables... filtrandolas de la mejor manera.
-            if(sector == "cumbaya"):
-                sector = "Cumbayá"
-            elif(sector == "puembo"):
-                sector = "Puembo"
-            
-            resultado = ml.entradas([
-                {
-                    'BuscarPor' : buscarPor,
-                    'Habitaciones' : habitaciones,
-                    'Parqueadero' : parqueadero,
-                    'TipoAcabados' : tipoAcabados,
-                    'Sector' : sector,
-                }
-            ])
+        if(habitaciones != "" and parqueadero != "" and tipoAcabados != ""):
+            if(sector != ""):
+                if(sector == "cumbaya" or sector == "puembo"):
+                    #Acomodamos nuestras variables... filtrandolas de la mejor manera.
+                    if(sector == "cumbaya"):
+                        sector = "Cumbayá"
+                    elif(sector == "puembo"):
+                        sector = "Puembo"
+
+                elif(sector == "centroNorte-quito" or sector == "norte-quito"):
+                    pass
 
 
-            if(buscarPor == "casas_venta"):
-                buscarPor = "Casas en Venta"
-            elif(buscarPor == "departamento_venta"):
-                buscarPor = "Departamentos en Venta"
+                resultado = ml.entradas([
+                    {
+                        'Habitaciones' : habitaciones,
+                        'Parqueadero' : parqueadero,
+                        'TipoAcabados' : tipoAcabados,
+                        'Sector' : sector,
+                    }
+                ])
 
-            #con esta entrada alimentamos a nuestro modelo...
-            datos['BuscarPor'] = buscarPor
-            datos['Habitaciones'] = habitaciones
-            datos['Parqueadero'] = parqueadero
-            datos['TipoAcabados'] = tipoAcabados
-            datos['Sector'] = sector
+                #con esta entrada alimentamos a nuestro modelo...
+                datos['Habitaciones'] = habitaciones
+                datos['Parqueadero'] = parqueadero
+                datos['TipoAcabados'] = tipoAcabados
+                datos['Sector'] = sector
 
-            if(resultado):
-                resultado = ml.ejecutarModelo()
-                entrada['nombre'] = resultado['nombre'].values
-                entrada['sector'] = resultado['sector'].values
-                entrada['precio'] = resultado['precio'].values
-                entrada['area'] = resultado['area'].values
-                entrada['habitaciones'] = resultado['habitaciones'].values
-                entrada['banos'] = resultado['banos'].values
-                entrada['parqueadero'] = resultado['parqueadero'].values
-                entrada['fecha'] = resultado['fecha'].values
-                entrada['ciudad'] = resultado['ciudad'].values 
-                entrada['precioXm2'] = resultado['precioXm2'].values
+                if(resultado):
+                    resultado = ml.ejecutarModelo()
+                    entrada['nombre'] = resultado['nombre'].values
+                    entrada['sector'] = resultado['sector'].values
+                    entrada['precio'] = resultado['precio'].values
+                    entrada['area'] = resultado['area'].values
+                    entrada['habitaciones'] = resultado['habitaciones'].values
+                    entrada['banos'] = resultado['banos'].values
+                    entrada['parqueadero'] = resultado['parqueadero'].values
+                    entrada['fecha'] = resultado['fecha'].values
+                    entrada['ciudad'] = resultado['ciudad'].values 
+                    entrada['precioXm2'] = resultado['precioXm2'].values
 
         else:
             #redireccionamos al usuarioo... pero le generamos un parametros para que pueda ser consumido despues.\\
